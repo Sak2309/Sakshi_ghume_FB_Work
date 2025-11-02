8a90f6aa2eabf5958b60df8aa8c122335fa6f15a
sakshi ghume <sakshighume5948@gmail.com>
2025-11-02 23:31:10 +0530

    Update Q12_palinder_no_or_not.py

--- Core Python/assigments/Assigment 3/Q12_palinder_no_or_not.py
+++ Core Python/assigments/Assigment 3/Q12_palinder_no_or_not.py
@@ -11,8 +11,9 @@ d3 = temp % 10
 temp = temp // 10
 rev = d1 * 100 + d2 * 10 + d3
 print(f"After the reverse number = {rev}")
-if( rev == rev):
+if( n == rev):
     print(f"PALINDER NUMBER.")
 else:
     print(f"NOT PALINDER NUMBER.")
+
 
