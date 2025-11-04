78990ce685102f13dabee428ed3a632c3376ff3d
sakshi ghume <sakshighume5948@gmail.com>
2025-11-04 19:53:41 +0530

    Delete Core Python/assigments/Assigment 8(function) directory

--- Core Python/assigments/Assigment 8(function)/q1_Area_Rectangle.py
+++
@@ -1,8 +0,0 @@
-def area_Rectangle():
-    
-    l = int(input("ENTER THE LENGTH OF RECTANGLE : "))
-    b = int (input("ENTER THE BREATH OF RECTANGLE : "))
-    area_rectangle = l * b
-    print(f" RECTANGLE of LENGTH IS {l} and BREATH IS {b} THE AREA OF RECTANGLE IS :{area_rectangle} ")
-    
-area_Rectangle()
--- Core Python/assigments/Assigment 8(function)/q2_Area_circle.py
+++
@@ -1,8 +0,0 @@
-def area_Circle():
-
-    r = int(input("ENTER THE REDIUS OF CIRCLE : "))
-    
-    area_circle = 3.14 * r*r
-    print(f"REDIUS OF CIRCLE IS {r} and ATREA OF CIRCLE IS : {area_circle}")
-
-area_Circle()
