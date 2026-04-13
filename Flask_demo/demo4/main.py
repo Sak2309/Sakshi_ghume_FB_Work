f146aa88f11ff4003b544c69b60e031c4b8de1d7
sakshi ghume <sakshighume5948@gmail.com>
2026-04-13 16:06:38 +0530

    Add route for displaying nested list of scores
    Added a new route to display a nested list of student scores.

--- Flask_demo/demo4/main.py
+++ Flask_demo/demo4/main.py
@@ -18,6 +18,16 @@ def displaylist():
     data = ["Sakshi", "saniya", "riya","priya","isha"]
     return render_template("displaylist.html", data=data)
 
+@app.route("/displaynestedlist")
+def displaynestedlist():
+    data = [
+        [101,"Sakshi", 88],
+        [102,"saniya", 92],
+        [103,"riya", 85],
+        [104,"priya", 90],
+        [105,"isha", 87]
+    ]
+    return render_template("displaynestedlist.html", data=data)     
 
 @app.route("/login", methods=["GET","POST"])
 def login():
